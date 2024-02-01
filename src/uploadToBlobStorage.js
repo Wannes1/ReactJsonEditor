61426ce1697c2b5e621f1f745681ee6d26e62c25
Wannes1 <wannes2306@live.be>
2024-02-01 16:39:22 +0100

    fetch instead of fs

--- src/uploadToBlobStorage.js
+++ src/uploadToBlobStorage.js
@@ -1,30 +1,39 @@
 import { BlobServiceClient } from '@azure/storage-blob';
-import fs from 'fs';
 
-// // Load environment variables from .env file in development
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config();
-// }
-
-const uploadFileToBlob = async (filePath) => {
+const uploadFileToBlob = async (file) => {
   const connectionString = 'DefaultEndpointsProtocol=https;AccountName=matchanalyticscvtest;AccountKey=otrKdAPEaQYnPadYwe5mybWlcEIe68rE+MUbbkrFLr2dVfB+ttC3P4m/RcP+J5mb7T1xDexZcO7z+AStEmdLNg==;EndpointSuffix=core.windows.net';
   const containerName = 'testjson';
   const blobName = 'wannestest.json';
-
+  
   const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionString);
   const containerClient = blobServiceClient.getContainerClient(containerName);
   const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
+  
   try {
     // Read the file content
-    const data = fs.readFileSync(filePath, 'utf-8');
+    const fileContent = await readFileAsync(file);
+  
     // Upload the file content to Azure Blob Storage
-    await blockBlobClient.upload(data, data.length);
+    await blockBlobClient.upload(fileContent, fileContent.length);
     console.log('File uploaded successfully to Azure Blob Storage!');
   } catch (error) {
     console.error('Error uploading file to Azure Blob Storage:', error.message);
     throw error;
   }
 };
-
+  
+// Helper function to read file asynchronously
+const readFileAsync = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      resolve(event.target.result);
+    };
+    reader.onerror = (error) => {
+      reject(error);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+};
+  
 export { uploadFileToBlob };
